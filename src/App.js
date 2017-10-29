8bc8023528b8056d72a2136ae67db00a78c53a41
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2017-10-29 16:48:58 -0600

    testing atom git functionality

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>
-          Hi, I'm a React App!
+          Hi, I'm a React App! 
         </h1>
       </div>
     );
