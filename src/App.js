1394ac6ec45ee1cd64ad1325af9e1583e6c9840e
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2017-10-29 20:13:57 -0600

    pause before learning about state

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ class App extends Component {
           Hi, I'm a React App!
         </h1>
         <p>This is really working!</p>
-        <Person />
+        <Person name="Max" age="28"/>
+        <Person name="Manu" age="29">My Hobbies: Racing</Person>
+        <Person name="Stephanie" age="26"/>
       </div>
     );
     // return React.createElement('div', null, React.createElement('h1',null,'Hi, I\'m a react ap!!!'))
