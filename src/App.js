85e8ec25c3688d55737a129423ab65c8da5a3353
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2017-10-31 13:13:25 -0600

    person css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,39 @@ import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+  state = {
+    persons: [
+      {Name: 'Max', age: 28},
+      {Name: 'Manu', age: 29},
+      {Name: 'Stephanie', age: 26}
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked');
+    this.setState(
+      {
+        persons: [
+          {Name: newName, age: 28},
+          {Name: 'Manu', age: 29},
+          {Name: 'Stephanie', age: 27}
+        ]
+      }
+    );
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState(
+      {
+        persons: [
+          {Name: 'Max', age: 28},
+          {Name: event.target.value, age: 29},
+          {Name: 'Stephanie', age: 26}
+        ]
+      }
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +43,25 @@ class App extends Component {
           Hi, I'm a React App!
         </h1>
         <p>This is really working!</p>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">My Hobbies: Racing</Person>
-        <Person name="Stephanie" age="26"/>
+        <button onClick={() => this.switchNameHandler('Maximilian!')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].Name}
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler}
+        />
+        <Person
+          name={this.state.persons[1].Name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangedHandler}
+          >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].Name}
+          age={this.state.persons[2].age}
+          click={this.switchNameHandler}
+        />
       </div>
     );
     // return React.createElement('div', null, React.createElement('h1',null,'Hi, I\'m a react ap!!!'))
