1279517af14c5514899db99b8298aecc908215ec
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2017-11-08 16:17:56 -0700

    restructuring/refactoring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
+import Persons from '../components/Persons/Person';
 import Person from '../components/Persons/Person/Person';
 
 class App extends Component {
@@ -49,15 +50,11 @@ class App extends Component {
     if ( this.state.showPersons ) {
       persons = (
         <div>
-          {this.state.persons.map((person, index) => {
-            return
-              <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangedHandler(event, person.id)} />
-          })}
+          <Persons
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler}
+          />
         </div>
       );
 
